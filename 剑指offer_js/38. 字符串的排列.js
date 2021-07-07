98bb157b02c6e3b82f42c359988767945db1abfc
马海涛 <mahaitao@photonpay.com>
2021-07-07 17:40:32 +0800

    股票最大利润

--- 剑指offer_js/38. 字符串的排列.js
+++ 剑指offer_js/38. 字符串的排列.js
@@ -1,3 +1,4 @@
+// 采用回朔法实现
 // 输入：s = "abc"
 // 输出：["abc","acb","bac","bca","cab","cba"]
 /**
@@ -6,29 +7,52 @@
  */
  let test = "abc"
  var permutation = function(s) {
-     let len = s.length
-     let result = []
-    //  for(let i =len;i>=0;i--) {
-    //     // result.push([s[s.length-i-1]])
-    //      for(let j = 0;j<s.length;j++) {
-    //         //  if(s[i] === s[j]) {
-    //         //      continue
-    //         //  }
-    //         console.log(s[i],s[j])
-    //         // console.log(result[i],s[j])
-    //      }
-    //  }
-    for(let i =0;i<s.length;i++) {
-        result.push([s[i]])
-        for(let j = 0;j<s.length;j++) {
-            if(s[i] === s[j]) {
-                continue
-            }
-            result[i].push(s[j])
+    const rec = [], vis = [];
+    const n = s.length;
+    const arr = Array.from(s).sort();
+    const perm = [];
+    const backtrack = (arr, i, n, perm) => {
+        if (i === n) {
+            rec.push(perm.toString());
+            return;
         }
-        
+        for (let j = 0; j < n; j++) {
+            if (vis[j] || (j > 0 && !vis[j - 1] && arr[j - 1] === arr[j])) {
+                continue;
+            }
+            vis[j] = true;
+            perm.push(arr[j]);
+            backtrack(arr, i + 1, n, perm);
+            perm.pop();
+            vis[j] = false;
+        }
     }
-     console.log(result)
-    //  result.push(s[s.length-i])
+
+    backtrack(arr, 0, n, perm);
+    const size = rec.length;
+    const recArr = new Array(size).fill(0);
+    for (let i = 0; i < size; i++) {
+        recArr[i] = rec[i].split(',').join('');
+    }
+    return recArr;
+};
+console.log(permutation(test))
+
+// 奇怪的实现方式
+let test = "abc"
+ var permutation = function(str) {
+    var result=[];
+    if(str.length==1){
+        return [str]
+    }else{
+        var preResult= permutation(str.slice(1));
+        for (var j = 0; j < preResult.length; j++) {
+            for (var k = 0; k < preResult[j].length+1; k++) {
+                var temp=preResult[j].slice(0,k)+str[0]+preResult[j].slice(k);
+                result.push(temp);
+            }
+        }
+        return result;
+    }
 };
 console.log(permutation(test))
